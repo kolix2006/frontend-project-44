7ff4fdab209a68f1e49920edf10049c30170cb69
kolix2006 <kolyanychkolya5@gmail.com>
2026-02-16 11:23:22 +0300

    linter added

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,9 @@
 import readlineSync from 'readline-sync'
 
 const sayHello = () => {
-    console.log('Welcome to the Brain Games!')
-    var userName = readlineSync.question('May I have your name? ')
-    console.log('Hello, ' + userName + '!')
+  console.log('Welcome to the Brain Games!')
+  const userName = readlineSync.question('May I have your name? ')
+  console.log('Hello, ' + userName + '!')
 }
 
 export { sayHello }
